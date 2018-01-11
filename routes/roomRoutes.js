38e140fde8256dda914736472171b8f6e4a7c939
tonio155 <tonio155@gmail.com>
2018-01-11 21:37:56 +0000

    removed request package from routes where not necessary

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { ObjectID } = require('mongodb');
-const request = require('request');
 const mongoose = require('mongoose');
 
 const User = require('../models/userModel');
