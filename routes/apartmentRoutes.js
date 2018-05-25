842a982fa0fbf6cc859750aeae58263d100e66b1
Antonio Rossi <info@antoniorossi.net>
2018-05-25 11:48:06 +0200

    api moved to async await

--- routes/apartmentRoutes.js
+++ routes/apartmentRoutes.js
@@ -8,96 +8,102 @@ let { authenticate } = require('./../middleware/auth');
 
 
 // GET all the apartments
-router.get('/', authenticate, (req, res) => {
-  const token = req.header('authorization').split(' ')[1];
-    
-  User.findByToken(token)
-    .then(user => {
-      // look up for all the apartments created by the current user
-      Apartment.find({ createdBy: ObjectID.ObjectId(user._id) })
-        .populate('rooms')
-        .populate('user')
-        .populate('updatedBy')
-        .then(apartments => res.send(apartments))
-        .catch(e => res.status(400).send(e));
-    });
+router.get('/', authenticate, async (req, res) => {
+  try {
+    const token = req.header('authorization').split(' ')[1];
+      
+    const user = await User.findByToken(token);
+    const apartment = await Apartment.find({ createdBy: ObjectID.ObjectId(user._id) })
+      .populate('rooms')
+      .populate('user')
+      .populate('updatedBy')
+
+      res.send({ message: 'List of apartments', api: 'GET/apartments', apartments});
+  } catch (e) {
+    res.status(400).send({ message: 'Error in retreving the apartments list', api: 'GET/apartments' })
+  }
 });
 
 // GET query of apartments
-router.get('/query', authenticate, (req, res) => {
-  const searchQuery = req.query;
-  // in the search term I want to be able to query both query with name or description, then query both with only one. So I take which has been queried and copy that across to query both, plus I query the label with the search term just for wider visibilty
-  if (req.query.name || req.query.description) {
-    req.query.$or = [];
-    req.query.$or.push({ name: new RegExp(req.query.name || req.query.description, 'i') });
-    req.query.$or.push({ name: new RegExp(req.query.name || req.query.description || req.query.location, 'i') });
-    req.query.$or.push({ description: new RegExp(req.query.location || req.query.description || req.query.name, 'i') });
-    req.query.$or.push({ 'label': new RegExp(req.query.description || req.query.name, 'i') });
-    delete req.query.name;
+router.get('/query', authenticate, async (req, res) => {
+  try {
+    const searchQuery = req.query;
+    // in the search term I want to be able to query both query with name or description, then query both with only one. So I take which has been queried and copy that across to query both, plus I query the label with the search term just for wider visibilty
+    if (req.query.name || req.query.description) {
+      req.query.$or = [];
+      req.query.$or.push({ name: new RegExp(req.query.name || req.query.description, 'i') });
+      req.query.$or.push({ name: new RegExp(req.query.name || req.query.description || req.query.location, 'i') });
+      req.query.$or.push({ description: new RegExp(req.query.location || req.query.description || req.query.name, 'i') });
+      req.query.$or.push({ 'label': new RegExp(req.query.description || req.query.name, 'i') });
+      delete req.query.name;
+    }
+    const mongoQuery = { $and: [{ }] };
+    mongoQuery.$and.push(searchQuery);
+  
+    const apartments = await Apartment.find(mongoQuery)
+      .populate('rooms')
+      .populate('user')
+      .populate('updatedBy');
+    
+      res.send({ message: 'List of queried appartments', api: 'GET/apartments/query', apartments })
+    } catch (e) {
+      res.status(400).send({ message: 'Error in revreving the list of apartments', api: 'GET/apartments/query', e});
   }
-  const mongoQuery = {
-    $and: [{
-      // refine search
-    }]
-  };
-  mongoQuery.$and.push(searchQuery);
-
-  Apartment.find(mongoQuery)
-    .populate('rooms')
-    .populate('user')
-    .populate('updatedBy')
-    .then(apartments => res.send({ apartments }))
-    .catch(e => res.status(400).send(e));
 });
 
 // Create a new apartment
-router.post('/', authenticate, (req, res) => {
-  const token = req.header('authorization').split(' ')[1];
-  const { body } = req;  
-
-  const apartment = new Apartment(body);
-
-  User.findByToken(token)
-    .then(user => {
-      apartment.createdBy = user;
-      return apartment.save();
-    })
-    .then(apartment => res.send({ message: 'The apartment has been created', apartment }))
-    .catch(e => res.status(400).send({ message: 'Unable to create the apartment', e }));
+router.post('/', authenticate, async (req, res) => {
+  try {
+    const token = req.header('authorization').split(' ')[1];
+    const { body } = req;  
+  
+    const user = await User.findByToken(token)
+    
+    const apartment = new Apartment(body);
+    apartment.createdBy = user;
+    await apartment.save();
+    
+    res.send({ message: 'The apartment has been created successully', api: 'POST/apartments', apartment });
+  } catch (e) {
+    res.status(400).send({ message: 'Unable to create the apartment', api: 'POST/apartments', e });
+  }
 });
 
 // UPDATE apartment
-router.patch('/:id', authenticate, (req, res) => {
-  const token = req.header('authorization').split(' ')[1];
-  const { id } = req.params;
-  const body = req;
-
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send({ error: 'ObjectID not valid' });
+router.patch('/:id', authenticate, async (req, res) => {
+  try {
+    const token = req.header('authorization').split(' ')[1];
+    const { id } = req.params;
+    const body = req;
+  
+    if (!ObjectID.isValid(id)) return res.status(404).send({ error: 'ObjectID not valid' });
+  
+    const user = await User.findByToken(token)
+    
+    const apartment = await Apartment.findByIdAndUpdate(id, {
+      $set: body,
+      updatedBy: user._id
+    }, { new: true });
+      
+    res.send({ message: 'Apartment updated successully', api: `PATCH/apartments/${id}`, apartment });
+  } catch (e) {
+    res.status(400).send({ message: 'Error in updating the apartment information', api: `PATCH/apartments/${id}`, e});  
   }
-
-  User.findByToken(token)
-    .then(user => {
-      return Apartment.findByIdAndUpdate(id, {
-        $set: body,
-        updatedBy: user._id
-      }, { new: true });
-    })
-    .then(apartment => res.send({ apartment }))
-    .catch(e => res.status(400).send(e));
 });
 
 // DELETE apartment
-router.delete('/:id', authenticate, (req, res) => {
-  const { id } = req.params;
-
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send({ error: 'ObjectID not valid' });
+router.delete('/:id', authenticate, async (req, res) => {
+  try {
+    const { id } = req.params;
+  
+    if (!ObjectID.isValid(id)) return res.status(404).send({ error: 'ObjectID not valid' });
+  
+    const result = await Apartment.findByIdAndRemove(id);
+    
+    res.send({ message: 'Apartment deleted successully', api: `DELETE/apartments/${id}`, result });
+  } catch (e) {
+    res.status(400).send({ status: 'error', message: 'Unable to delete the apartment', e});
   }
-
-  Apartment.findByIdAndRemove(id)
-    .then(result => res.send({ status: 'success', message: 'Apartment Deleted', result })
-    .catch(e => res.status(400).send({ status: 'error', message: 'Unable to delete the apartment', e})));
 });
 
 module.exports = router;
--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const emailHandler = require('../config/email');
 
-router.post('/', (req, res) => {
-  emailHandler.sendEmail(req);
-  res.status(200).send({ message: 'email sent' });
+router.post('/', async (req, res) => {
+  try {
+    await ßemailHandler.sendEmail(req);
+    res.status(200).send({ message: 'email sent' });
+  } catch (e) {
+    res.status(400).send({ message: 'Error in sending the email', api: 'POST/email', e })
+  }
 });
 
 module.exports = router;
